130b7ef18049b7c12db48dbd7dd7b6c4becd27f6
Nirmal Todwal <162917515+nirmaltodwal7@users.noreply.github.com>
2025-03-09 00:12:50 +0530

    Add files via upload

---
+++ nirmal1.js
@@ -0,0 +1,17 @@
+let firstName = "harkirat";
+let age = 18;
+let isMarried = false;
+
+console.log("this person name is " + firstName + "and their age is " + age)
+
+console.log("this persons name is")
+console.log(firstName);
+
+
+if (isMarried == true) {
+    console.log(firstName + " is married");
+}
+
+if (isMarried == false) {
+    console.log(firstName + " is not married");
+}
